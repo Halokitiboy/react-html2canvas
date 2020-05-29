db029418c280325a9a6408c3ce9bc8e5266a6df0
夏正春 <xiazhengchun_job@126.com>
2020-05-29 10:49:35 +0800

    feat: 🎸 新增构建目录

---
+++ docs/precache-manifest.e98a597cdd0bb18dab743798d67c1517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5905153ab703dbc244cf1f81bea6c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64b28b358ddaed40d2ef",
+    "url": "/static/css/2.e5641e0e.chunk.css"
+  },
+  {
+    "revision": "09d5a2981646577f283b",
+    "url": "/static/css/main.aafd783e.chunk.css"
+  },
+  {
+    "revision": "64b28b358ddaed40d2ef",
+    "url": "/static/js/2.13a0306d.chunk.js"
+  },
+  {
+    "revision": "b2dded7b94c3ca00d250fdc3f43e92e2",
+    "url": "/static/js/2.13a0306d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d5a2981646577f283b",
+    "url": "/static/js/main.584a0ff0.chunk.js"
+  },
+  {
+    "revision": "c73fb9a366435c70ed44",
+    "url": "/static/js/runtime-main.6e869f0b.js"
+  }
+]);
